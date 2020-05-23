61668fd5dcbd6a7ed212d4d8c694ae1f3a9e167b
wordpython <wordpython@163.com>
2020-05-24 02:01:58 +0800

    第一次上传ydl学院前端前台系统“

---
+++ layouts/error.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <div class="ll">
+      <h2>业务研发中....</h2>
+      <h3>请耐心等待喔~亲</h3>
+      <h3>系统会第一时间通知您</h3>
+      <nuxt-link to="/"><p class="ls">首 页</p></nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      default: null
+    }
+  },
+  layout: 'sign' // 你可以为错误页面指定自定义的布局
+}
+</script>
+<style>
+.ll {
+  padding:5% 30%;
+}
+.ls{
+  font-size: 28px;
+  margin-top: 30px;
+  margin-left: 30px;
+}
+</style>
+
---
+++ pages/index.vue
@@ -0,0 +1,220 @@
+<template>
+
+  <div>
+    <!-- 幻灯片 开始 -->
+    <div v-swiper:mySwiper="swiperOption">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide" style="background: #040B1B;">
+          <a target="_blank" href="/">
+            <img src="~/assets/photo/banner/1525939573202_.png" alt="首页banner">
+          </a>
+        </div>
+        <div class="swiper-slide" style="background: #040B1B;">
+          <a target="_blank" href="/">
+            <img src="~/assets/photo/banner/1525939573202_.png" alt="首页banner">
+          </a>
+        </div>
+      </div>
+      <div class="swiper-pagination swiper-pagination-white"></div>
+      <div slot="button-prev" class="swiper-button-prev swiper-button-white"></div>
+      <div slot="button-next" class="swiper-button-next swiper-button-white"></div>
+    </div>
+    <!-- 幻灯片 结束 -->
+
+    <div id="aCoursesList">
+      <!-- 网校课程 开始 -->
+      <div>
+        <section class="container">
+          <header class="comm-title">
+            <h2 class="tac">
+              <span class="c-333">热门课程</span>
+            </h2>
+          </header>
+          <div>
+            <article class="comm-course-list">
+              <ul id="bna" class="of">
+                <li v-for="item in data.data.items" :key="item.id">
+                  <div class="cc-l-wrap">
+                    <section class="course-img">
+                      <img
+                        :src="item.cover"
+                        :alt="item.title"
+                        class="img-responsive"
+                      >
+                      <div class="cc-mask">
+                        <a :href="'/course/'+item.id" title="开始学习" class="comm-btn c-btn-1">开始学习</a>
+                      </div>
+                    </section>
+                    <h3 class="hLh30 txtOf mt10">
+                      <a :href="'/course/'+item.id" :title="item.title" class="course-title fsize18 c-333">{{ item.title }}</a>
+                    </h3>
+                    <section class="mt10 hLh20 of">
+                      <span class="fr jgTag bg-green">
+                        <i class="c-fff fsize12 f-fA">免费</i>
+                      </span>
+                      <span class="fl jgAttr c-ccc f-fA">
+                        <i class="c-999 f-fA">{{ item.viewCount }}人学习</i>
+                        |
+                        <i class="c-999 f-fA">9634评论</i>
+                      </span>
+                    </section>
+                  </div>
+                </li>
+              </ul>
+              <div class="clear"/>
+            </article>
+            <section class="tac pt20">
+              <a href="/course" title="全部课程" class="comm-btn c-btn-2">全部课程</a>
+            </section>
+          </div>
+        </section>
+      </div>
+      <!-- /网校课程 结束 -->
+      <!-- 网校名师 开始 -->
+      <div>
+        <section class="container">
+          <header class="comm-title">
+            <h2 class="tac">
+              <span class="c-333">名师大咖</span>
+            </h2>
+          </header>
+          <div>
+            <article class="i-teacher-list">
+              <ul class="of">
+                <li>
+                  <section class="i-teach-wrap">
+                    <div class="i-teach-pic">
+                      <a href="/teacher/6" title="姚晨">
+                        <img alt="姚晨" src="https://ydl-edu-file.oss-cn-shenzhen.aliyuncs.com/avatar/2020/05/18/83604ad2-3aaf-4948-aec6-d71e5e7afcc9.png">
+                      </a>
+                    </div>
+                    <div class="mt10 hLh30 txtOf tac">
+                      <a href="/teacher/6" title="姚晨" class="fsize18 c-666">姚晨</a>
+                    </div>
+                    <div class="hLh30 txtOf tac">
+                      <span class="fsize14 c-999">北京师范大学法学院副教授</span>
+                    </div>
+                    <div class="mt15 i-q-txt">
+                      <p
+                        class="c-999 f-fA"
+                      >北京师范大学法学院副教授、清华大学法学博士。自2004年至今已有9年的司法考试培训经验。长期从事司法考试辅导，深知命题规律，了解解题技巧。内容把握准确，授课重点明确，层次分明，调理清晰，将法条法理与案例有机融合，强调综合，深入浅出。</p>
+                    </div>
+                  </section>
+                </li>
+                <li>
+                  <section class="i-teach-wrap">
+                    <div class="i-teach-pic">
+                      <a href="/teacher/9" title="谢老师">
+                        <img alt="谢老师" src="~/assets/photo/teacher/02.jpg">
+                      </a>
+                    </div>
+                    <div class="mt10 hLh30 txtOf tac">
+                      <a href="/teacher/9" title="谢老师" class="fsize18 c-666">谢老师</a>
+                    </div>
+                    <div class="hLh30 txtOf tac">
+                      <span class="fsize14 c-999">资深课程设计专家，专注10年AACTP美国培训协会认证导师</span>
+                    </div>
+                    <div class="mt15 i-q-txt">
+                      <p
+                        class="c-999 f-fA"
+                      >十年课程研发和培训咨询经验，曾任国企人力资源经理、大型外企培训经理，负责企业大学和培训体系搭建；曾任专业培训机构高级顾问、研发部总监，为包括广东移动、东莞移动、深圳移动、南方电网、工商银行、农业银行、民生银行、邮储银行、TCL集团、清华大学继续教育学院、中天路桥、广西扬翔股份等超过200家企业提供过培训与咨询服务，并担任近50个大型项目的总负责人。</p>
+                    </div>
+                  </section>
+                </li>
+                <li>
+                  <section class="i-teach-wrap">
+                    <div class="i-teach-pic">
+                      <a href="/teacher/10" title="唐老师">
+                        <img alt="唐老师" src="https://ydl-edu-file.oss-cn-shenzhen.aliyuncs.com/avatar/2020/05/18/7c140a96-c3f7-4a64-98f0-8047602eb4ee.png">
+                      </a>
+                    </div>
+                    <div class="mt10 hLh30 txtOf tac">
+                      <a href="/teacher/10" title="唐老师" class="fsize18 c-666">唐老师</a>
+                    </div>
+                    <div class="hLh30 txtOf tac">
+                      <span class="fsize14 c-999">北京师范大学法学院副教授</span>
+                    </div>
+                    <div class="mt15 i-q-txt">
+                      <p
+                        class="c-999 f-fA"
+                      >北京师范大学法学院副教授、清华大学法学博士。自2004年至今已有9年的司法考试培训经验。长期从事司法考试辅导，深知命题规律，了解解题技巧。内容把握准确，授课重点明确，层次分明，调理清晰，将法条法理与案例有机融合，强调综合，深入浅出。</p>
+                    </div>
+                  </section>
+                </li>
+                <li>
+                  <section class="i-teach-wrap">
+                    <div class="i-teach-pic">
+                      <a href="/teacher/4" title="周老师">
+                        <img alt="周老师" src="https://ydl-edu-file.oss-cn-shenzhen.aliyuncs.com/avatar/2020/05/18/953bf71d-2e3f-423c-ab58-9c3f9fdb6e60.png">
+                      </a>
+                    </div>
+                    <div class="mt10 hLh30 txtOf tac">
+                      <a href="/teacher/4" title="周老师" class="fsize18 c-666">周老师</a>
+                    </div>
+                    <div class="hLh30 txtOf tac">
+                      <span class="fsize14 c-999">考研政治辅导实战派专家，全国考研政治命题研究组核心成员。</span>
+                    </div>
+                    <div class="mt15 i-q-txt">
+                      <p
+                        class="c-999 f-fA"
+                      >法学博士，北京师范大学马克思主义学院副教授，专攻毛泽东思想概论、邓小平理论，长期从事考研辅导。出版著作两部，发表学术论文30余篇，主持国家社会科学基金项目和教育部重大课题子课题各一项，参与中央实施马克思主义理论研究和建设工程项目。</p>
+                    </div>
+                  </section>
+                </li>
+              </ul>
+              <div class="clear"/>
+            </article>
+            <section class="tac pt20">
+              <a href="/teacher" title="全部讲师" class="comm-btn c-btn-2">全部讲师</a>
+            </section>
+          </div>
+        </section>
+      </div>
+      <!-- /网校名师 结束 -->
+    </div>
+  </div>
+</template>
+
+<script>
+import course from '@/api/course'
+import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
+import 'swiper/css/swiper.css'
+export default {
+  name: 'SwiperExampleLoop',
+  title: 'Loop mode / Infinite loop',
+  components: {
+    Swiper,
+    SwiperSlide
+  },
+  data() {
+    return {
+      swiperOption: {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        autoplay: true,
+        loop: true,
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev'
+        }
+      }
+    }
+  },
+  asyncData({ params, error }) {
+    return course.getHotCourse().then(response => {
+      console.log(response.data.data)
+      return { data: response.data }
+    })
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  // @import './base.scss';
+</style>
